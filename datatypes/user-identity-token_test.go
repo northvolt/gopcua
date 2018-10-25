4f7efa0d7943cc858417a35915b61b2d9ed7436c
Frank Schroeder <frank.schroeder@northvolt.com>
2018-10-25 14:22:58 +0200

    refactor TestAnonymousIdentityToken

--- datatypes/user-identity-token_test.go
+++ datatypes/user-identity-token_test.go
@@ -7,77 +7,27 @@ package datatypes
 import (
 	"testing"
 
+	"github.com/wmnsk/gopcua/utils/codectest"
 	"github.com/google/go-cmp/cmp"
 )
 
-var anonymousIdentityTokenCases = []struct {
-	description string
-	structured  *AnonymousIdentityToken
-	serialized  []byte
-}{
-	{
-		"normal",
-		NewAnonymousIdentityToken("anonymous"),
-		[]byte{
-			0x09, 0x00, 0x00, 0x00,
-			0x61, 0x6e, 0x6f, 0x6e, 0x79, 0x6d, 0x6f, 0x75, 0x73,
+func TestAnonymousIdentityToken(t *testing.T) {
+	cases := []codectest.Case{
+		{
+			Name:   "normal",
+			Struct: NewAnonymousIdentityToken("anonymous"),
+			Bytes: []byte{
+				0x09, 0x00, 0x00, 0x00,
+				0x61, 0x6e, 0x6f, 0x6e, 0x79, 0x6d, 0x6f, 0x75, 0x73,
+			},
 		},
-	},
-}
-
-func TestDecodeAnonymousIdentityToken(t *testing.T) {
-	for _, c := range anonymousIdentityTokenCases {
-		got, err := DecodeAnonymousIdentityToken(c.serialized)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if diff := cmp.Diff(got, c.structured); diff != "" {
-			t.Errorf("%s failed\n%s", c.description, diff)
-		}
 	}
+	codectest.Run(t, cases, func(b []byte) (codectest.S, error) {
+		return DecodeAnonymousIdentityToken(b)
+	})
 }
 
-func TestSerializeAnonymousIdentityToken(t *testing.T) {
-	for _, c := range anonymousIdentityTokenCases {
-		got, err := c.structured.Serialize()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if diff := cmp.Diff(got, c.serialized); diff != "" {
-			t.Errorf("%s failed\n%s", c.description, diff)
-		}
-	}
-}
-
-func TestAnonymousIdentityTokenLen(t *testing.T) {
-	for _, c := range anonymousIdentityTokenCases {
-		got := c.structured.Len()
-
-		if diff := cmp.Diff(got, len(c.serialized)); diff != "" {
-			t.Errorf("%s failed\n%s", c.description, diff)
-		}
-	}
-}
-
-var userNameIdentityTokenCases = []struct {
-	description string
-	structured  *UserNameIdentityToken
-	serialized  []byte
-}{
-	{
-		"normal",
-		NewUserNameIdentityToken("username", "user", []byte{0x70, 0x61, 0x73, 0x73, 0x77, 0x6f, 0x72, 0x64}, "plain"),
-		[]byte{
-			// PolicyID
-			0x08, 0x00, 0x00, 0x00, 0x75, 0x73, 0x65, 0x72, 0x6e, 0x61, 0x6d, 0x65,
-			// UserName
-			0x04, 0x00, 0x00, 0x00, 0x75, 0x73, 0x65, 0x72,
-			// Password
-			0x08, 0x00, 0x00, 0x00, 0x70, 0x61, 0x73, 0x73, 0x77, 0x6f, 0x72, 0x64,
-			// EncryptionAlgorithm
-			0x05, 0x00, 0x00, 0x00, 0x70, 0x6c, 0x61, 0x69, 0x6e,
+func TestUserNameIdentityToken(t *testing.T) {
 		},
 	},
 }
